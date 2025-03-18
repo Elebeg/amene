7d7fdf51fb4c1949f36f78c2beb00c5836706c0b
Lucas Schuler Foppa <lucasfoppa@hotmail.com>
2025-03-18 14:42:56 -0300

    Feaure: Mudança de imagem

--- script.js
+++ script.js
@@ -211,5 +211,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+
+    document.getElementById("contactForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+        
+        alert("Formulário enviado com sucesso! Entraremos em contato em breve.");
+        this.reset();
+    });
+
 });
 
